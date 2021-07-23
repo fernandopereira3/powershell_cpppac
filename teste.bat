6304dfdd34fa3224c3a70d2dfaa826ee3bb8ae1d
fernandopereira3 <39241138+fernandopereira3@users.noreply.github.com>
2021-07-23 13:59:21 -0300

    Delete teste.bat

--- teste.bat
+++
@@ -1,3 +0,0 @@
-$interface = (Get-NetIPConfiguration | Where-Object {($_.InterfaceAlias -CContains "Ethernet")} | Select-Object InterfaceIndex) 
-Set-DnsClientServerAddress -InterfaceIndex $interface -ServerAddresses "10.1.6.222","ù192.168.10.10"ù,"ù10.14.180.2"ù,"ù10.14.180.10"
-echo $interface
